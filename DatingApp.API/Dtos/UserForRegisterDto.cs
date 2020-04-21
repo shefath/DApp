5a56e577090203e0a37e5228f19f14fcac6c06fb
Sheik Maideen <shefath@hotmail.com>
2020-04-21 12:31:06 +0530

    End of Section3

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 6, ErrorMessage = "You must specify password between 6 and 8")]
+        public string Password { get; set; }
+    }
+}

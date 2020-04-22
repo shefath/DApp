7f34f852858bbf47a6318ea3deca765e079d116e
Sheik Maideen <shefath@hotmail.com>
2020-04-22 10:37:59 +0530

    End of Section 4

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { NavComponent } from "./components/nav/nav.component";
 import { ValueComponent } from "./value/value.component";
 
+import { AuthService } from "./_services/auth.service";
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, ValueComponent, NavComponent, HomeComponent, RegisterComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ DatingApp-SPA/src/app/components/register/register.component.ts
@@ -0,0 +1,35 @@
+import { AuthService } from "./../../_services/auth.service";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  register() {
+    console.log(this.model);
+    this.authService.register(this.model).subscribe(
+      () => {
+        console.log("Registration Successful");
+        this.model = {};
+        this.cancelRegister.emit(false);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    this.model = {};
+  }
+}
